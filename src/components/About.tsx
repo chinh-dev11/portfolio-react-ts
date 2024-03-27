6022e8426a32bc2bf0948c78729d98e777d8eb0b
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-03-27 10:40:02 -0400

    integrate about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,13 @@
 import '../styles/About.css'
+import { about } from '../data'
 
 const About = () => {
   return (
-    <section className='about' id="section3">About</section>
+    <section className='about' id="section3">
+      <img src={about.image.src} alt={about.image.title} />
+      <h2>{about.title}</h2>
+      <p>{about.text}</p>
+    </section>
   )
 }
 
