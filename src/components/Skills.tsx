f7f6884285916215fc2b89289f6f32a9339f3732
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-04-03 16:16:56 -0400

    add skills icons and colors

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,11 +1,75 @@
+import { ReactNode } from "react"
+import { FaVuejs, FaReact, FaAngular, FaJs, FaHtml5, FaCss3, FaSass, FaLess, FaNpm, FaYarn, FaNodeJs, FaAws } from 'react-icons/fa'
+import { SiTypescript, SiJest, SiCypress, SiGraphql, SiMysql, SiFirebase } from 'react-icons/si'
+import { IoIosGitBranch } from "react-icons/io"
 import useFetchData from '../services/fetchData'
-import { FaHtml5, FaJs, FaReact } from 'react-icons/fa'
 import { BlockProps } from '../types'
 
 interface SkillProps {
   title: string,
   text: string,
   icon: string
+}
+
+const iconSize = 'w-16 h-16'
+
+const createIconNode = (skill: SkillProps): ReactNode | undefined => {
+  const cssClasses = `${iconSize} text-${skill.icon}`
+
+  const iconList: any = {
+    vue: <FaVuejs className={cssClasses} title={skill.title} />,
+    react: <FaReact className={cssClasses} title={skill.title}/>,
+    ng: <FaAngular className={cssClasses} title={skill.title}/>,
+    js: <FaJs className={cssClasses} title={skill.title}/>,
+    ts: <SiTypescript className={cssClasses} title={skill.title}/>,
+    html: <FaHtml5 className={cssClasses} title={skill.title}/>,
+    css: <FaCss3 className={cssClasses} title={skill.title}/>,
+    sass: <FaSass className={cssClasses} title={skill.title}/>,
+    less: <FaLess className={cssClasses} title={skill.title}/>,
+    jest: <SiJest className={cssClasses} title={skill.title}/>,
+    cy: <SiCypress className={cssClasses} title={skill.title}/>,
+    npm: <FaNpm className={cssClasses} title={skill.title}/>,
+    yarn: <FaYarn className={cssClasses} title={skill.title}/>,
+    nodejs: <FaNodeJs className={cssClasses} title={skill.title}/>,
+    gql: <SiGraphql className={cssClasses} title={skill.title}/>,
+    mysql: <SiMysql className={cssClasses} title={skill.title}/>,
+    firebase: <SiFirebase className={cssClasses} title={skill.title}/>,
+    aws: <FaAws className={cssClasses} title={skill.title}/>,
+    git: <IoIosGitBranch className={cssClasses} title={skill.title}/>
+  }
+  
+  return iconList[skill.icon]
+}
+
+const createSkillsNodes = (skillsData: SkillProps[]): ReactNode => {
+  const skillsIcons: ReactNode[] = []
+  const skillsTitles: ReactNode[] = []
+
+  skillsData.forEach(skill => {
+    const skillIcon = createIconNode(skill)
+
+    if (!skillIcon) skillsTitles.push(<span className="capitalize">{skill.title}</span>)
+    else skillsIcons.push(skillIcon)
+  })
+
+  return (
+    <div>
+      {skillsIcons.length && (
+        <ul className="flex flex-row flex-wrap gap-4 mb-5">
+          {skillsIcons.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      )}
+      {skillsTitles.length && (
+        <ul className="flex flex-row flex-wrap gap-4">
+          {skillsTitles.map((skill, index) => (
+            <li key={index} className="bg-gray-200 p-2 rounded">{skill}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
 }
 
 export default () => {
@@ -19,20 +83,7 @@ export default () => {
   return (
     <section className="py-20 align-element" id="section2">
       <h2 className="text-3xl font-bold capitalize border-b border-gray-300 pb-5 mb-16">{blockData.title}</h2>
-      <ul className="flex flex-row flex-wrap gap-5">
-        {skillsData.map(({ title, icon }, index) => {
-          return (
-            <li key={index}>
-              <span title={title}>
-                {icon === 'html' && <FaHtml5 className="w-20 h-20 text-html" />}
-                {icon === 'js' && <FaJs className="w-20 h-20 text-js" />}
-                {icon === 'react' && <FaReact className="w-20 h-20 text-react" />}
-                {icon === '' && title}
-              </span>
-            </li>
-          )
-        })}
-      </ul>
+      {createSkillsNodes(skillsData)}
     </section>
   )
 }
--- src/helpers/techIconsColors.js
+++ src/helpers/techIconsColors.js
@@ -1,5 +1,21 @@
 export default {
-  html: '#de4b25',
+  vue: '#42b883',
+  react: '#47d2f8',
+  ng:'#d7002f',
   js: '#e9d21b',
-  react: '#47d2f8'
+  ts:'#2f75c0',
+  html: '#de4b25',
+  css:'#254bde',
+  sass:'#c86496',
+  less:'#234877',
+  jest:'#944058',
+  cy:'#59595a',
+  npm:'#dd2d34',
+  yarn:'#2d8ab5',
+  nodejs:'#2d8ab5',
+  gql:'#df33a6',
+  mysql:'#5a839c',
+  firebase:'#f3c425',
+  aws:'#212d3c',
+  git:'#ea4f30'
 }
