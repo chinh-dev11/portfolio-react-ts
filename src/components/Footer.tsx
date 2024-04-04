2c265884ba0e0d8661cc82d75f6b7160cd6e270c
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-04-04 19:42:23 -0400

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
+import useFetchData from '../services/fetchData'
+
 export default () => {
+  const { loading, data: { text } } = useFetchData('footer')
+
+  if (loading) return <div>Loading...</div>
+
+  console.log(text)
+
   return (
-    <footer className="p-5 flex flex-row justify-center">
-      <span className="text-slate-300">&copy; 2024 Chinh Le. All rights reserved.</span>
+    <footer className="px-10 py-5 flex flex-row justify-center">
+      <span className="text-slate-300">&copy; {text}</span>
     </footer>
   )
 }
