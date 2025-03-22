40e4c79fb2188479692c77a8238bfe6670644fca
Chinh <chinh.dev@icloud.com>
2025-03-22 17:20:51 -0400

    Add gitlab icon for link list item

--- src/helpers/createLinksList.tsx
+++ src/helpers/createLinksList.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react"
 import { LinkProps } from '../types'
-import { FaGithubSquare, FaLinkedin } from 'react-icons/fa'
+import { FaGithubSquare, FaGitlab, FaLinkedin } from 'react-icons/fa'
 import { TbWorldWww } from 'react-icons/tb'
 
 export default (links: LinkProps[]): ReactNode => {
@@ -11,6 +11,7 @@ export default (links: LinkProps[]): ReactNode => {
           <a href={link.url} target="_blank" title={link.title} rel="noreferrer">
             <span>
               {link.icon === 'github' && <FaGithubSquare className="w-8 h-8 text-slate-500 hover:text-black duration-300" />}
+              {link.icon === 'gitlab' && <FaGitlab className="w-8 h-8 text-slate-500 hover:text-black duration-300" />}
               {link.icon === 'linkedin' && <FaLinkedin className="w-8 h-8 text-slate-500 hover:text-black duration-300" />}
               {link.icon === 'www' && <TbWorldWww className="w-8 h-8 text-slate-500 hover:text-black duration-300" />}
               {link.icon === '' && link.title}
